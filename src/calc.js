9d37509ee5dc20845a6646da3d5cb380c194407e
alekhyajaddu <alekhyajaddu@gmail.com>
2020-03-07 14:21:29 -0600

    Made changes in code

--- src/calc.js
+++ src/calc.js
@@ -1,5 +1,15 @@
 function temperatureConverter(valNum) {
         valNum = parseFloat(valNum);
-        document.getElementById("outputCelcius").innerHTML=valNum-273.15;
+        const result=(valNum-32) / 1.8;
+        document.getElementById("outputCelcius").innerHTML=result;
+
+         //localstorage to store number of times the converter was used
+         if (localStorage.counter) {
+          localStorage.counter = Number(localStorage.counter) + 1;
+          }
+        else {
+         localStorage.counter = 1;
+          }
+          document.getElementById("numOfConverts").innerHTML = localStorage.counter;
       }
       
---
+++ test/calc.test.js
@@ -0,0 +1,31 @@
+QUnit.module('MAIN MODULE', {})  
+
+QUnit.test('TEST', assert => {
+    function temperatureConverter(valNum) {
+        return (valNum-32) / 1.8;
+    } 
+    
+    QUnit.test( "Test", function( assert ) {
+       var result = temperatureConverter(2);
+       assert.equal( result, "-16.666666666666668", "2 fahrenheit should be -16.666666666666668 celcius." );
+    });
+    QUnit.test( "Test", function( assert ) {
+       var result = temperatureConverter(70);
+       assert.equal( result, "21.11111111111111", "70 fahrenheit should be  21.11111111111111 celcius." );
+    });
+    QUnit.test( "Test", function( assert ) {
+       var result = temperatureConverter(5);
+       assert.equal( result, "-15", "5 fahrenheit should be -15 celcius." );
+    });
+    QUnit.test( "Test", function( assert ) {
+       var result = temperatureConverter(100);
+       assert.equal( result, "37.77777777777778", "100 fahrenheit should be 37.77777777777778 celcius." );
+    });
+
+    QUnit.test( "Test", function( assert ) {
+        var result = temperatureConverter(46);
+        assert.equal( result, "7.777777777777778", "46 fahrenheit should be 7.777777777777778 celcius." );
+     });
+     
+})
+
